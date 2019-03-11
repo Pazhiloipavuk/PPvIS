546209b96318ce0e9171652758953a2ad16f72eb
Pavel Kalenik <proevklid@gmail.com>
2019-03-11 08:28:52 +0300

    Last version

--- Lab1/src/Window.java
+++ Lab1/src/Window.java
@@ -1,225 +1,384 @@
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.*;
-import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.graphics.Rectangle;
+
 
 public class Window {
-	
 	public void solveTasks(Shell shell) {
-	    Group group1 = new Group(shell, SWT.SHADOW_IN);
-	    group1.setText("TASK¹1");
-	    group1.setLayout(new RowLayout(SWT.HORIZONTAL));
-		Text textOfGroup1 = new Text(group1, SWT.BORDER);
-		textOfGroup1.setLayoutData(new RowData());
-		Combo combo = new Combo(group1, SWT.DROP_DOWN | SWT.READ_ONLY);
-		Button buttonOfGroup1 = new Button(group1, SWT.PUSH);
-		buttonOfGroup1.setText("Button");
+		Group firstGroup = new Group(shell, SWT.SHADOW_IN);
+		firstGroup.setText("TASK¹1");
+		firstGroup.setSize(250, 120);
+
+		Text textFGr = new Text(firstGroup, SWT.BORDER);
+		textFGr.setBounds(0, 20, 60, 20);
+
+		Combo combo = new Combo(firstGroup, SWT.DROP_DOWN | SWT.READ_ONLY);
+		combo.setBounds(0, 60, 60, 20);
 		
-		buttonOfGroup1.addSelectionListener(new SelectionAdapter() {
-			
+		Button pushButtonFGr = new Button(firstGroup, SWT.PUSH);
+		pushButtonFGr.setText("Button");
+		pushButtonFGr.setBounds(0, 100, 50, 20);
+		pushButtonFGr.addSelectionListener(new SelectionAdapter() {
+
 			public void widgetSelected(SelectionEvent arg0) {
-				if (textOfGroup1.getText() == null || textOfGroup1.getText().length() == 0) {
+				if (textFGr.getText() == null || textFGr.getText().length() == 0) {
 					return;
-	            }
-	            String[] items = combo.getItems();
-	            for (String item : items) {
-	            	if (item.equals(textOfGroup1.getText())) {
-	            		showMessage(shell);
-	                }
-	            }
-				combo.add(textOfGroup1.getText());
-				textOfGroup1.setText("");
-				textOfGroup1.forceFocus();
-			}
-		});
-		
-	    Group group2 = new Group(shell, SWT.SHADOW_IN);
-	    group2.setText("TASK¹2");
-	    group2.setLayout(new RowLayout(SWT.HORIZONTAL));
-		Text textOfGroup2 = new Text(group2, SWT.BORDER);
-		textOfGroup2.setLayoutData(new RowData());
-		Button button1OfGroup2 = new Button(group2, SWT.PUSH);
-		button1OfGroup2.setText("Button1");
-		Button button2OfGroup2 = new Button(group2, SWT.PUSH);
-		button2OfGroup2.setText("Button2");
-		
-		button1OfGroup2.addSelectionListener(new SelectionAdapter() {
-			
-			public void widgetSelected(SelectionEvent arg0) {
-				button2OfGroup2.setText(textOfGroup2.getText());
-				textOfGroup2.setText("");
-				textOfGroup2.forceFocus();
-			}
-		});
-		
-		button2OfGroup2.addSelectionListener(new SelectionAdapter() {
-			
-			public void widgetSelected(SelectionEvent arg0) {
-				textOfGroup2.setText(button1OfGroup2.getText());
-				button1OfGroup2.setText(button2OfGroup2.getText());
-				button2OfGroup2.setText(textOfGroup2.getText());
-				textOfGroup2.setText("");
-			}
-		});
-
-	    Group group3 = new Group(shell, SWT.SHADOW_IN);
-	    group3.setText("TASK¹3");
-	    group3.setLayout(new RowLayout(SWT.HORIZONTAL));
-	    Group groupRadioButton = new Group(group3, SWT.SHADOW_IN);
-	    groupRadioButton.setLayout(new RowLayout(SWT.VERTICAL));
-	    Button radiobutton1 = new Button(groupRadioButton, SWT.RADIO);
-	    radiobutton1.setText("R1");
-	    Button radiobutton2 = new Button(groupRadioButton, SWT.RADIO);
-	    radiobutton2.setText("R2");
-	    Button radiobutton3 = new Button(groupRadioButton, SWT.RADIO);
-	    radiobutton3.setText("R3");
-		Text textOfGroup3 = new Text(group3, SWT.BORDER);
-		textOfGroup3.setLayoutData(new RowData());
-		Button buttonOfGroup3 = new Button(group3, SWT.PUSH);
-		buttonOfGroup3.setText("Button");
-		
-		buttonOfGroup3.addSelectionListener(new SelectionAdapter() {
-			
-			public void widgetSelected(SelectionEvent arg0) {
-				if (textOfGroup3.getText().equals(radiobutton1.getText())) {
-					radiobutton1.setSelection(true);
-					radiobutton2.setSelection(false);
-					radiobutton3.setSelection(false);
-				} else if (textOfGroup3.getText().equals(radiobutton2.getText())) {
-					radiobutton1.setSelection(false);	
-					radiobutton2.setSelection(true);
-					radiobutton3.setSelection(false);
-				} else if (textOfGroup3.getText().equals(radiobutton3.getText())) {
-					radiobutton1.setSelection(false);	
-					radiobutton2.setSelection(false);		
-					radiobutton3.setSelection(true);			
-				} else {
-					showMessage(shell);
-					textOfGroup3.setText("");
-					textOfGroup3.forceFocus();
-	            }
-				textOfGroup3.setText("");
-				textOfGroup3.forceFocus();
-			}
-		});
-		
-		Group group4 = new Group(shell, SWT.SHADOW_IN);
-	    group4.setText("TASK¹4");
-	    group4.setLayout(new RowLayout(SWT.HORIZONTAL));
-	    Group groupCheckButton = new Group(group4, SWT.SHADOW_IN);
-	    groupCheckButton.setLayout(new RowLayout(SWT.VERTICAL));
-	    Button checkbutton1 = new Button(groupCheckButton, SWT.CHECK);
-	    checkbutton1.setText("C1");
-	    Button checkbutton2 = new Button(groupCheckButton, SWT.CHECK);
-	    checkbutton2.setText("C2");
-	    Button checkbutton3 = new Button(groupCheckButton, SWT.CHECK);
-	    checkbutton3.setText("C3");
-		Text textOfGroup4 = new Text(group4, SWT.BORDER);
-		textOfGroup4.setLayoutData(new RowData());
-		Button buttonOfGroup4 = new Button(group4, SWT.PUSH);
-		buttonOfGroup4.setText("Button");
-		
-		buttonOfGroup4.addSelectionListener(new SelectionAdapter() {
-			
-			public void widgetSelected(SelectionEvent arg0) {
-				if (textOfGroup4.getText().equals(checkbutton1.getText())) {
-					checkbutton1.setSelection(!checkbutton1.getSelection());;
-				} else if (textOfGroup4.getText().equals(checkbutton2.getText())) {
-					checkbutton2.setSelection(!checkbutton2.getSelection());
-				} else if (textOfGroup4.getText().equals(checkbutton3.getText())) {	
-					checkbutton3.setSelection(!checkbutton3.getSelection());			
-				} else {
-					showMessage(shell);
-					textOfGroup4.setText("");
-					textOfGroup4.forceFocus();
 				}
-				textOfGroup4.setText("");
-				textOfGroup4.forceFocus();
+				String[] items = combo.getItems();
+				for (String item : items) {
+					if (item.equals(textFGr.getText())) {
+						showErrorMessage(shell);
+					}
+				}
+				combo.add(textFGr.getText());
+				clearText(textFGr);
+			}
+		});
+		
+		ellipseLayout(firstGroup);
+		
+		Group secondGroup = new Group(shell, SWT.SHADOW_IN);
+		secondGroup.setText("TASK¹2");
+		secondGroup.setSize(250, 120);
+
+		Text textSGr = new Text(secondGroup, SWT.BORDER);
+		textSGr.setBounds(0, 20, 60, 20);
+
+		Button firstPushButtonSGr = new Button(secondGroup, SWT.PUSH);
+		firstPushButtonSGr.setText("Button1");
+		firstPushButtonSGr.setBounds(0, 60, 50, 20);
+
+		Button secondPushButtonSGr = new Button(secondGroup, SWT.PUSH);
+		secondPushButtonSGr.setText("Button2");
+		secondPushButtonSGr.setBounds(0, 100, 50, 20);
+
+		firstPushButtonSGr.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent arg0) {
+				secondPushButtonSGr.setText(textSGr.getText());
+				clearText(textSGr);
 			}
 		});
 
-	    Group group5 = new Group(shell, SWT.SHADOW_IN);
-	    group5.setText("TASK¹5");
-	    group5.setLayout(new RowLayout(SWT.HORIZONTAL));
-	    Table table = new Table(group5, SWT.MULTI | SWT.BORDER |SWT.FULL_SELECTION);
-	    RowData layoutTable = new RowData();
-	    layoutTable.width=160;
-	    layoutTable.height=100;
-	    table.setLinesVisible(true); 
-	    table.setHeaderVisible(true);
-	    table.setLayoutData(layoutTable);
-	    TableColumn column1 = new TableColumn(table, SWT.DEFAULT);
-	    column1.setText("First column");
-	    column1.pack();
-	    TableColumn column2 = new TableColumn(table, SWT.DEFAULT);
-	    column2.setText("Second column");
-	    column2.pack();
-		Text textOfGroup5 = new Text(group5, SWT.BORDER);
-		textOfGroup5.setLayoutData(new RowData());
-		Button button1OfGroup5 = new Button(group5, SWT.PUSH);
-		button1OfGroup5.setText("Button");
+		secondPushButtonSGr.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent arg0) {
+				textSGr.setText(firstPushButtonSGr.getText());
+				firstPushButtonSGr.setText(secondPushButtonSGr.getText());
+				secondPushButtonSGr.setText(textSGr.getText());
+				clearText(textSGr);
+			}
+		});
 		
-		button1OfGroup5.addSelectionListener(new SelectionAdapter() {
-			
+		ellipseLayout(secondGroup);
+		
+		Group thirdGroup = new Group(shell, SWT.SHADOW_IN);
+		thirdGroup.setText("TASK¹3");
+		thirdGroup.setSize(210, 120);
+
+		Group groupRadioButton = new Group(thirdGroup, SWT.SHADOW_IN);
+		groupRadioButton.setBounds(10, 20, 60, 80);
+
+		Button firstRadioButton = new Button(groupRadioButton, SWT.RADIO);
+		firstRadioButton.setText("R1");
+		firstRadioButton.setBounds(10, 15, 40, 20);
+
+		Button secondRadionButton = new Button(groupRadioButton, SWT.RADIO);
+		secondRadionButton.setText("R2");
+		secondRadionButton.setBounds(10, 35, 40, 20);
+
+		Button thirdRadioButton = new Button(groupRadioButton, SWT.RADIO);
+		thirdRadioButton.setText("R3");
+		thirdRadioButton.setBounds(10, 55, 40, 20);
+
+		Text textTGr = new Text(thirdGroup, SWT.BORDER);
+		textTGr.setBounds(75, 27, 60, 20);
+
+		Button pushButtonTGr = new Button(thirdGroup, SWT.PUSH);
+		pushButtonTGr.setText("Button");
+		pushButtonTGr.setBounds(140, 27, 50, 20);
+
+		pushButtonTGr.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent arg0) {
+				if (textTGr.getText().equals(firstRadioButton.getText())) {
+					firstRadioButton.setSelection(true);
+					secondRadionButton.setSelection(false);
+					thirdRadioButton.setSelection(false);
+				} else if (textTGr.getText().equals(secondRadionButton.getText())) {
+					firstRadioButton.setSelection(false);
+					secondRadionButton.setSelection(true);
+					thirdRadioButton.setSelection(false);
+				} else if (textTGr.getText().equals(thirdRadioButton.getText())) {
+					firstRadioButton.setSelection(false);
+					secondRadionButton.setSelection(false);
+					thirdRadioButton.setSelection(true);
+				} else {
+					showErrorMessage(shell);
+					clearText(textTGr);
+				}
+				clearText(textTGr);
+			}
+		});
+		
+		Group fourthGroup = new Group(shell, SWT.SHADOW_IN);
+		fourthGroup.setText("TASK¹4");
+		fourthGroup.setSize(210, 120);
+
+		Group groupCheckButton = new Group(fourthGroup, SWT.SHADOW_IN);
+		groupCheckButton.setBounds(10, 20, 60, 80);
+
+		Button firstCheckButton = new Button(groupCheckButton, SWT.CHECK);
+		firstCheckButton.setText("1");
+		firstCheckButton.setBounds(10, 15, 40, 20);
+		
+		Button secondCheckButton = new Button(groupCheckButton, SWT.CHECK);
+		secondCheckButton.setText("2");
+		secondCheckButton.setBounds(10, 35, 40, 20);
+
+		Button thirdCheckButton = new Button(groupCheckButton, SWT.CHECK);
+		thirdCheckButton.setText("3");
+		thirdCheckButton.setBounds(10, 55, 40, 20);
+
+		Text textFoGr = new Text(fourthGroup, SWT.BORDER);
+		textFoGr.setBounds(75, 27, 60, 20);
+
+		Button pushButtonFoGr = new Button(fourthGroup, SWT.PUSH);
+		pushButtonFoGr.setText("Button");
+		pushButtonFoGr.setBounds(140, 27, 50, 20);
+
+		pushButtonFoGr.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent arg0) {
+				if (textFoGr.getText().equals(firstCheckButton.getText())) {
+					firstCheckButton.setSelection(!firstCheckButton.getSelection());;
+				} else if (textFoGr.getText().equals(secondCheckButton.getText())) {
+					secondCheckButton.setSelection(!secondCheckButton.getSelection());;
+				} else if (textFoGr.getText().equals(thirdCheckButton.getText())) {
+					thirdCheckButton.setSelection(!thirdCheckButton.getSelection());;
+				} else {
+					showErrorMessage(shell);
+					clearText(textFoGr);
+				}
+				clearText(textFoGr);
+			}
+		});
+		
+		Group fifthGroup = new Group(shell, SWT.SHADOW_IN);
+		fifthGroup.setText("TASK¹5");
+		fifthGroup.setSize(300, 300);
+
+		Table table = new Table(fifthGroup, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+		table.setBounds(10, 15, 190, 160);
+
+		TableColumn firstColumn = new TableColumn(table, SWT.DEFAULT);
+		firstColumn.setText("First column");
+		firstColumn.pack();
+
+		TableColumn secondColumn = new TableColumn(table, SWT.DEFAULT);
+		secondColumn.setText("Second column");
+		secondColumn.pack();
+
+		Text textFiGr = new Text(fifthGroup, SWT.BORDER);
+		textFiGr.setBounds(15, 180, 50, 20);
+
+		Button firstPushButtonFiGr = new Button(fifthGroup, SWT.PUSH);
+		firstPushButtonFiGr.setText("Button");
+		firstPushButtonFiGr.setBounds(15, 210, 50, 20);
+
+		firstPushButtonFiGr.addSelectionListener(new SelectionAdapter() {
+
 			public void widgetSelected(SelectionEvent arg0) {
 				TableItem item = new TableItem(table, SWT.DEFAULT, 0);
-				item.setText(0, textOfGroup5.getText());
-				textOfGroup5.setText("");	
+				item.setText(0, textFiGr.getText());
+				clearText(textFiGr);
 			}
 		});
-		
-		Button button2OfGroup5 = new Button(group5, SWT.PUSH);
-		button2OfGroup5.setText("Button1");
 
-		button2OfGroup5.addSelectionListener(new SelectionAdapter() {
-			
-			public void widgetSelected(SelectionEvent arg0) {
-				TableItem item = table.getSelection()[0];
-            	if (item.getText(0).equals("")) {
-            		return;
-                } else {
-				item.setText(1, item.getText(0));
-				item.setText(0, "");
-                }
-			}
-		});
-		
-		Button button3OfGroup5 = new Button(group5, SWT.PUSH);
-		button3OfGroup5.setText("Button2");
+		Button secondPushButtonFiGr = new Button(fifthGroup, SWT.PUSH);
+		secondPushButtonFiGr.setText("Button1");
+		secondPushButtonFiGr.setBounds(15, 240, 50, 20);
 
-		button3OfGroup5.addSelectionListener(new SelectionAdapter() {
-			
+		secondPushButtonFiGr.addSelectionListener(new SelectionAdapter() {
+
 			public void widgetSelected(SelectionEvent arg0) {
-				TableItem item = table.getSelection()[0];
-            	if (item.getText(1).equals("")) {
-            		return;
-                } else {
-				item.setText (0, item.getText(1));
-				item.setText (1, "");
-                }
+				moveItem(table, 0);
 			}
 		});
+
+		Button thirdPushButton = new Button(fifthGroup, SWT.PUSH);
+		thirdPushButton.setText("Button2");
+		thirdPushButton.setBounds(15, 270, 50, 20);
+
+		thirdPushButton.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent arg0) {
+				moveItem(table, 1);
+			}
+		});
+		ellipseLayout(shell);
 	}
-	
-	public void showMessage(Shell shell) {		
+
+	public void showErrorMessage(Shell shell) {
 		MessageBox message = new MessageBox(shell, SWT.ICON_ERROR);
 		message.setText("ERROR!");
 		message.setMessage("Unable!");
 		message.open();
 		return;
 	}
+
+	public void clearText(Text text) {
+		text.setText("");
+		text.forceFocus();
+	}
+	
+	public void moveItem(Table table, int numColumn) {
+		TableItem item = table.getSelection()[0];
+		if (item.getText(numColumn).equals("")) {
+			return;
+		} else {
+			if (numColumn == 0) {
+				item.setText(1, item.getText(numColumn));
+				item.setText(0, "");
+			} else {
+				item.setText(0, item.getText(numColumn));
+				item.setText(1, "");
+			}
+		}
+	}
+	
+	public void ellipseLayout(Shell shell) {
+		boolean upperHalfPlane = true;
+		int intX = 0;
+		int intY = 0;
+		int a = 0;
+		int b = 0;
+		int step = 0;
+		double doubleX = 0;
+		double doubleY = 0;
 		
-	public static void main(String [] args) {		
+		Control[] arrayShell = shell.getChildren();
+		
+		Rectangle screen = shell.getBounds();
+		a = screen.width/2;
+		b = screen.height/2;
+		step = screen.width/arrayShell.length;
+		
+	    for (int i = 0; i < arrayShell.length; i++) {
+	    	Rectangle widget = arrayShell[i].getBounds();
+	    	
+	    	if (doubleX <= a) {
+	        	doubleY = Math.sqrt((a*a - doubleX*doubleX)*b*b/(a*a));
+	    	} else {
+	           	doubleY = Math.sqrt((doubleX*doubleX - a*a)*b*b/(a*a));
+	    	}
+	    	
+	    	if (upperHalfPlane == true) {
+	        	intX = (int)doubleX;
+	        	intY = (int)doubleY;
+	    	} else {
+	        	intX = (int)doubleX;
+	        	intY = (int)(screen.height - doubleY);
+	    	}
+	    	
+			if (intX == 0) {
+		 		arrayShell[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX + 2*step;
+		 		upperHalfPlane = true;
+			} else if (intX == screen.width) {
+				arrayShell[i].setLocation(intX - widget.width, intY - widget.height/2);
+				doubleX = doubleX - 2*step;
+				upperHalfPlane = false;
+			} else if (intX + 2*step > screen.width) {
+				arrayShell[i].setLocation(intX, intY - widget.height/2);
+				doubleX = doubleX - 2*step;
+				upperHalfPlane = false;
+			} else if (intX + 2*step > a) {
+		 		arrayShell[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX + step;
+			} else if (upperHalfPlane == false & intX - 2*step < a) {
+		 		arrayShell[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX - step;
+			} else if (upperHalfPlane == false) {
+		 		arrayShell[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX - 2*step;
+			}
+	    }
+	}
+	
+	public void ellipseLayout(Group group) {
+		boolean upperHalfPlane = true;
+		int intX = 0;
+		int intY = 0;
+		int a = 0;
+		int b = 0;
+		int step = 0;
+		double doubleX = 0;
+		double doubleY = 0;
+		
+		Control[] arrayGroup = group.getChildren();
+		
+		Rectangle screen = group.getBounds();
+		a = screen.width/2;
+		b = screen.height/2;
+		step = screen.width/arrayGroup.length - 24;
+		
+	    for (int i = 0; i < arrayGroup.length; i++) {
+	    	Rectangle widget = arrayGroup[i].getBounds();
+	    	
+	    	if (doubleX <= a) {
+	        	doubleY = Math.sqrt((a*a - doubleX*doubleX)*b*b/(a*a));
+	    	} else {
+	           	doubleY = Math.sqrt((doubleX*doubleX - a*a)*b*b/(a*a));
+	    	}
+	    	
+	    	if (upperHalfPlane == true) {
+	        	intX = (int)doubleX;
+	        	intY = (int)doubleY;
+	    	} else {
+	        	intX = (int)doubleX;
+	        	intY = (int)(screen.height - doubleY);
+	    	}
+	    	
+			if (intX == 0) {
+		 		arrayGroup[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX + 2*step;
+		 		upperHalfPlane = true;
+			} else if (intX == screen.width) {
+				arrayGroup[i].setLocation(intX - widget.width, intY - widget.height/2);
+				doubleX = doubleX - 2*step;
+				upperHalfPlane = false;
+			} else if (intX + 2*step > screen.width) {
+				arrayGroup[i].setLocation(intX, intY - widget.height/2);
+				doubleX = doubleX - 2*step;
+				upperHalfPlane = false;
+			} else if (intX + 2*step > a) {
+		 		arrayGroup[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX + step;
+			} else if (upperHalfPlane == false & intX - 2*step < a) {
+		 		arrayGroup[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX - step;
+			} else if (upperHalfPlane == false) {
+		 		arrayGroup[i].setLocation(intX, intY - widget.height/2);
+		 		doubleX = doubleX - 2*step;
+			}
+	    }
+	}
+
+	public static void main(String[] args) {
 		Display display = new Display();
-		Shell shell = new Shell(display);
+		Shell shell = new Shell(display, SWT.MAX);
+		shell.setText("Lab Work ¹1");
+		shell.setBounds(display.getClientArea ());
+		shell.open();
 		new Window().solveTasks(shell);
-		shell.setLayout(new RowLayout(SWT.VERTICAL));
-		shell.pack();
-		shell.open ();
-        shell.setText("Lab Work ¹1");
 		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) display.sleep();
+			if (!display.readAndDispatch())
+				display.sleep();
 		}
 		display.dispose();
 	}
